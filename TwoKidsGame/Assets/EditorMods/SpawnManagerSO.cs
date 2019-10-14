fed0e370fe824209f27b106af2f57a62c5dbe998
oded <oded56@gmail.com>
2019-10-14 13:19:34 +0300

    Added the base of the framework that will allow us to create level configurations and load scenes

---
+++ TwoKidsGame/Assets/EditorMods/SpawnManagerSO.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/SpawnManagerSO", order = 1)]
+public class SpawnManagerSO : ScriptableObject
+{
+    #region Floor
+    public string floorPrefabName;
+    public Vector3 roomDimentions;
+    public Vector3 roomCenter;
+    #endregion
+
+    #region Environment
+    #endregion
+
+    #region Player
+    #endregion
+
+    #region Populate
+    #endregion
+}
---
+++ TwoKidsGame/Assets/LevelInstantiator.cs
@@ -0,0 +1,60 @@
+﻿/// Project:        Two Kids In A Trench Coat - Game
+/// Authors:        Oded Nachshon
+/// Script:         LevelInstantiator
+/// Description:    This Script sould load information from
+///                 a pre-specified configuration and load
+///                 the level accordingly.
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelInstantiator : MonoBehaviour
+{
+    [SerializeField]
+    private SpawnManagerSO levelConf;
+
+    // Create the level
+    void Start()
+    {
+        if (levelConf == null)
+        {
+            throw new Exception("No level configuration defined for this level. Dumbasses.");
+            /// Further down the line we want that
+            /// handling a "no level conf exception"
+            /// would be loading some default config
+            /// instead of just popping out an error message
+        }
+        else
+        {
+            CreateFloor();
+            CreateEnvironment();
+            SpawnPlayers();
+            PopulateRoom();
+        }
+    }
+
+    // Get and intitialize the floor this level according to the level config
+    private void CreateFloor()
+    {
+        UnityEngine.Object pPrefab = Resources.Load("Prefabs/" + levelConf.floorPrefabName);
+        GameObject pNewObject = (GameObject)GameObject.Instantiate(pPrefab, levelConf.roomCenter, Quaternion.identity);
+        pNewObject.transform.localScale = levelConf.roomDimentions;
+    }
+
+    private void CreateEnvironment()
+    {
+        Debug.LogWarning("Not implemented");
+    }
+
+    private void SpawnPlayers()
+    {
+        Debug.LogWarning("Not implemented");
+    }
+
+    private void PopulateRoom()
+    {
+        Debug.LogWarning("Not implemented");
+    }
+}
