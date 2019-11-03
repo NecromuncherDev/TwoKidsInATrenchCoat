01c8defabca55b8aa573774b111b03fe7a8dfec4
BeardedAngel <oded56@gmail.com>
2019-11-03 22:03:10 +0200

    vgvgygvuvi

---
+++ TwoKidsGame/Assets/BottomKidMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottomKidMovement : MonoBehaviour
+{
+    private Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * Time.deltaTime);
+    }
+}
--- TwoKidsGame/Assets/Scripts/HandsControl.cs
+++ TwoKidsGame/Assets/Scripts/HandsControl.cs
@@ -15,21 +15,21 @@ public class HandsControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //if (Input.GetKey(KeyCode.RightArrow))
-        //{
-        //    rb.AddForceAtPosition((Vector3.right + Vector3.up) * 0.5f, this.transform.position, ForceMode.Impulse);
-        //}
-        //if (Input.GetKey(KeyCode.LeftArrow))
-        //{
-        //    rb.AddForceAtPosition((Vector3.left + Vector3.down) * 0.5f, this.transform.position, ForceMode.Impulse);
-        //}
-        //if (Input.GetKey(KeyCode.UpArrow))
-        //{
-        //    rb.AddForceAtPosition(Vector3.forward, this.transform.position, ForceMode.Impulse);
-        //}
-        //if (Input.GetKey(KeyCode.DownArrow))
-        //{
-        //    rb.AddForceAtPosition(Vector3.back, this.transform.position, ForceMode.Impulse);
-        //}
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.AddForceAtPosition((Vector3.right + Vector3.up) * 0.5f, this.transform.position, ForceMode.Impulse);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.AddForceAtPosition((Vector3.left + Vector3.down) * 0.5f, this.transform.position, ForceMode.Impulse);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.AddForceAtPosition(Vector3.forward, this.transform.position, ForceMode.Impulse);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rb.AddForceAtPosition(Vector3.back, this.transform.position, ForceMode.Impulse);
+        }
     }
 }
